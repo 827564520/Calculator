305f4fcbf227777f500f3bf5574cebf6636ef290
LiliCode <lili2011050508@163.com>
2016-06-13 23:40:23 +0800

    Initial Commit

---
+++ Calculator-C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Calculator-C
+//
+//  Created by 李立 on 16/6/13.
+//  Copyright © 2016年 李立. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
